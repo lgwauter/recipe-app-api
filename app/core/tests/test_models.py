b4e14ec470521e3cb93dad29080428805e48eb52
Laurens Wauters <laurens.wauters@robovision.eu>
2021-07-29 17:32:35 +0200

    linting fixes

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -26,7 +27,7 @@ class ModelTests(TestCase):
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
-            # Anything here should raise a ValueError, otherwise the test fails.
+            # Anything here should raise a ValueError
             get_user_model().objects.create_user(None, 'test123')
 
     def test_create_new_superuser(self):
